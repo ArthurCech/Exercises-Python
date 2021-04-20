acc9bccce43890dff6a8311ab9cc8c9e7d1468dd
Arthur Cech Francisco <68647632+ArthurCech@users.noreply.github.com>
2021-04-20 11:35:42 -0300

    Update exer01.py

--- pdf 2/exer01.py
+++ pdf 2/exer01.py
@@ -1,7 +1,7 @@
 # Escreva um programa que leia um número real X e apresente na tela a mensagem 
 # "Maior que zero" caso ocorra X > 0. Se X for menor ou igual a zero não faça nada.
 
-X = int(input('digite o valor de X: '))
+X = float(input('digite o valor de X: '))
 
 if X > 0:
     print('Maior que zero')
