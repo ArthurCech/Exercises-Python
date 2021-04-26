9274cd8c90819087d6d4b89c7ee4a48a86181c28
Arthur Cech Francisco <68647632+ArthurCech@users.noreply.github.com>
2021-04-26 18:10:25 -0300

    Delete Curso em Video - Desafios/Mundo 1 directory

--- Curso em Video - Desafios/Mundo 1/exer013.py
+++
@@ -1,8 +0,0 @@
-# faça um algoritmo que leia um salário de um funcionário e mostre seu novo salário com 15% de aumento
-
-salary = float(input('digite o salário do funcionário: '))
-
-# newSalary = salary + (salary * 15 / 100)
-newSalary = salary * 1.15
-
-print('\033[1;32mSalário com aumento de 15%: R$ {:.2f}\033[m'.format(newSalary))
--- Curso em Video - Desafios/Mundo 1/exer030.py
+++
@@ -1,8 +0,0 @@
-# crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR
-
-number = int(input('digite o número: '))
-
-if number % 2 == 0:
-    print('\033[1;33mO número {} é par\033[m'.format(number))
-else:
-    print('\033[1;34mO número {} é ímpar\033[m'.format(number))
