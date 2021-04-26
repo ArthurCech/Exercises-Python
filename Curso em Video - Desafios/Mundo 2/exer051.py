004bd6dbd8b2e3c705d1d640fa8363072b6ccf75
Arthur Cech Francisco <68647632+ArthurCech@users.noreply.github.com>
2021-04-26 18:10:33 -0300

    Delete Curso em Video - Desafios/Mundo 2 directory

--- Curso em Video - Desafios/Mundo 2/exer051.py
+++
@@ -1,9 +0,0 @@
-# desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10
-# primeiros termos dessa progressão
-
-firstTerm = int(input('digite o primeiro termo: '))
-commonDifference  = int(input('digite a razão: '))
-lastTerm = firstTerm + (10 - 1) * commonDifference
-
-for i in range(firstTerm, lastTerm + commonDifference, commonDifference):
-    print('{}'.format(i))
