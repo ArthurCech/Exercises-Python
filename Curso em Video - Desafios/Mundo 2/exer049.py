d1d1c0e33b3559ccb0e1ccbefe4518eb2c730777
Arthur Cech Francisco <68647632+ArthurCech@users.noreply.github.com>
2021-05-03 18:36:03 -0300

    Delete Curso em Video - Desafios/Mundo 2 directory

--- Curso em Video - Desafios/Mundo 2/exer049.py
+++
@@ -1,8 +0,0 @@
-# refaça o desafio 009 mostrando a tabuada de um número que o usuário escolher, só que 
-# agora utilizando um laço for
-
-number = int(input('digite o número que deseja ver a tabuada: '))
-print('-' * 11)
-for i in range(1, 11):
-    print('{} x {:2} = {}'.format(number, i, number * i))
-print('-' * 11)
