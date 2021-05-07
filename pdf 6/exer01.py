5a7a9bd86e43272a19ab1ecff4db3d908f634e75
Arthur Cech Francisco <68647632+ArthurCech@users.noreply.github.com>
2021-05-07 17:46:26 -0300

    Delete pdf 6 directory

--- pdf 6/exer01.py
+++
@@ -1,9 +0,0 @@
-# Escreva um programa que leia do teclado uma lista com tamanho de 10 elementos e exiba-o na tela um elemento por linha.
-
-listNumbers = []
-for i in range(10):
-    number = int(input('digite um número inteiro: '))
-    listNumbers.append(number)
-
-for number in listNumbers:
-    print(number)
